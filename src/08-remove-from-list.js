c08af39082ee6967f719db3f46ffadd28ed76d8b
Aziz <68072877+AbdurakhimovAziz@users.noreply.github.com>
2021-04-12 14:44:26 +0500

    feat: solve 8th task

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -7,7 +7,7 @@
  * @return {List}
  *
  * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
+ * For l = [3, 1, 2, 3, 4, 5] and l = 3,
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined with this interface
@@ -17,8 +17,24 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  let list = l;
+  let current = l;
+  let prev = null;
+
+  while (current) {
+    if (current.value === k) {
+      if (prev) {
+        prev.next = current.next;
+      } else {
+        list = current.next;
+      }
+    }
+    prev = current;
+    current = current.next;
+  }
+
+  return list;
 }
 
 module.exports = removeKFromList;
